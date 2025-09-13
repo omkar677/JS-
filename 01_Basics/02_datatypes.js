9c2cdfe9d28893a87928aec8deab99b9e59842ea
omkar677 <sutar.omkar2903@gmail.com>
2025-09-13 15:25:20 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name =  "abc"
+let age = 18
+let loggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string = ""
+// boolean => true/false 
+// null => standalone value 
+// undefined 
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); //Output => undefined 
+console.log(typeof null); //Output => object 
+
